1122916f49e1c3c8def69cb95d85c638b3c1683d
rasheedaldeb <rasheedaldeb@gmail.com>
2026-07-18 21:50:32 +0300

    implement rest data provider

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,6 +1,11 @@
 import { BACKEND_BASE_URL } from "@/constants";
 import { ListResponse } from "@/types";
 import { createDataProvider, CreateDataProviderOptions } from "@refinedev/rest";
+if (!BACKEND_BASE_URL) {
+  throw new Error(
+    "BACKEND_BASE_URL is not configure, please set VITE_BACKEND_BASE_URL in your .env file",
+  );
+}
 const options: CreateDataProviderOptions = {
   getList: {
     getEndpoint: ({ resource }) => resource,
@@ -19,11 +24,11 @@ const options: CreateDataProviderOptions = {
       return params;
     },
     mapResponse: async (response) => {
-      const payload: ListResponse = await response.json();
+      const payload: ListResponse = await response.clone().json();
       return payload.data ?? [];
     },
     getTotalCount: async (response) => {
-      const payload: ListResponse = await response.json();
+      const payload: ListResponse = await response.clone().json();
       return payload.pagination?.total ?? payload.data?.length ?? 0;
     },
   },
